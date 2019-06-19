a7f10c0e3a7b4ebd727b08c682fcfa4799817410
can1001 <hyejong.jeong@gmail.com>
2019-06-20 01:44:03 +0900

    make Movie Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       Hello, React!!
+        <Movie />
       </div>
     );
   }
---
+++ src/Movie.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Movie.css';
+
+class Movie extends Component {
+    render() {
+        return (
+            <div className="Movie">
+                <h1>Hello this is a movie</h1>
+            </div>
+        );
+    }
+}
+
+export default Movie;
