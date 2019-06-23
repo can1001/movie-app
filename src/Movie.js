8ba7acaa625b9738aff28372bf865103205463d7
can1001 <hyejong.jeong@gmail.com>
2019-06-24 01:01:02 +0900

    add Movie tag

--- src/Movie.js
+++ src/Movie.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import './Movie.css';
 
+const movies = [
+    title="StarWars",
+    aaa="StarWars",
+    bbb="StarWars",
+    ccc="StarWars"
+]
 class Movie extends Component {
     render() {
         return (
             <div className="Movie">
-                <h1>Hello this is a movie</h1>
+                <MoviePoster></MoviePoster>
+                <h1>StarWors</h1>
             </div>
+        );
+    }
+}
+
+class MoviePoster extends Component {
+    render() {
+        return(
+            <img src="http://image.yes24.com/momo/TopCate1291/MidCate003/129025467.jpg" width="150"></img>
         );
     }
 }
