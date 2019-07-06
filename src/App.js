be8cbcc9d4ee497cadd58237e66eca8435621e26
can1001 <hyejong.jeong@gmail.com>
2019-07-06 18:44:15 +0900

    componentDidMount -greeting

--- src/App.js
+++ src/App.js
@@ -25,9 +25,26 @@ const movies = [
   }
 ]
 class App extends Component {
+  // Render: componentWillMount() -> render() -> componentDidMount()
+  // Update: componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> rener() -> componentDidUpdate()
+
+  state = {
+    greeting: 'Hello!'
+
+  }
+
+  componentDidMount() {
+    setTimeout( () => {
+      this.setState({
+        greeting: 'something'
+      })
+    }, 5000)
+  }
+
   render() {
     return (
       <div className="App">
+        {this.state.greeting}
         {movies.map((movie, index) => {
           return <Movie title={movie.title} poster={movie.poster} key={index} />
         })}
