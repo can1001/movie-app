58566fea4156519163f00b328fed2e933af3cc40
can1001 <hyejong.jeong@gmail.com>
2019-07-06 20:46:46 +0900

    리액트 기초 배우기 #11

--- src/App.js
+++ src/App.js
@@ -2,41 +2,47 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie.js';
 
-const movies = [
-  {
-    id: 1,
-    title: "Matrix",
-    poster: "http://ticketimage.interpark.com/Movie/still_image/V16/V1601447p_s01.gif"
-  },
-  {
-    id: 2,
-    title: "Full Metal Jacket",
-    poster: "https://images-na.ssl-images-amazon.com/images/I/41VXPrZfDXL.jpg"
-  },
-  {
-    id: 3,
-    title: "Old Boy",
-    poster: "https://images-na.ssl-images-amazon.com/images/I/51U%2BG%2BZgGgL.jpg"
-  },
-  {
-    id: 4,
-    title: "Star Wars",
-    poster: "http://image.yes24.com/momo/TopCate1291/MidCate003/129025467.jpg"
-  }
-]
 class App extends Component {
   // Render: componentWillMount() -> render() -> componentDidMount()
   // Update: componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> rener() -> componentDidUpdate()
 
   state = {
-    greeting: 'Hello!'
-
+    greeting: 'Hello!',
+    movies: [
+      {
+        id: 1,
+        title: "Matrix",
+        poster: "http://ticketimage.interpark.com/Movie/still_image/V16/V1601447p_s01.gif"
+      },
+      {
+        id: 2,
+        title: "Full Metal Jacket",
+        poster: "https://images-na.ssl-images-amazon.com/images/I/41VXPrZfDXL.jpg"
+      },
+      {
+        id: 3,
+        title: "Old Boy",
+        poster: "https://images-na.ssl-images-amazon.com/images/I/51U%2BG%2BZgGgL.jpg"
+      },
+      {
+        id: 4,
+        title: "Star Wars",
+        poster: "http://image.yes24.com/momo/TopCate1291/MidCate003/129025467.jpg"
+      }
+    ]
   }
 
   componentDidMount() {
     setTimeout( () => {
       this.setState({
-        greeting: 'something'
+        movies: [
+          ...this.state.movies,
+          {
+            id: 4,
+            title: "Star Wars",
+            poster: "http://image.yes24.com/momo/TopCate1291/MidCate003/129025467.jpg"
+          }
+        ]
       })
     }, 5000)
   }
@@ -45,7 +51,7 @@ class App extends Component {
     return (
       <div className="App">
         {this.state.greeting}
-        {movies.map((movie, index) => {
+        {this.state.movies.map((movie, index) => {
           return <Movie title={movie.title} poster={movie.poster} key={index} />
         })}
       </div>
