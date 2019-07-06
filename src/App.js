8132bb4f3080f2807ae1cb0539cc9cd0328fa9bf
can1001 <hyejong.jeong@gmail.com>
2019-07-06 23:32:10 +0900

    리액트 기초 배우기 #12 Loading states

--- src/App.js
+++ src/App.js
@@ -7,36 +7,27 @@ class App extends Component {
   // Update: componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> rener() -> componentDidUpdate()
 
   state = {
-    greeting: 'Hello!',
-    movies: [
-      {
-        id: 1,
-        title: "Matrix",
-        poster: "http://ticketimage.interpark.com/Movie/still_image/V16/V1601447p_s01.gif"
-      },
-      {
-        id: 2,
-        title: "Full Metal Jacket",
-        poster: "https://images-na.ssl-images-amazon.com/images/I/41VXPrZfDXL.jpg"
-      },
-      {
-        id: 3,
-        title: "Old Boy",
-        poster: "https://images-na.ssl-images-amazon.com/images/I/51U%2BG%2BZgGgL.jpg"
-      },
-      {
-        id: 4,
-        title: "Star Wars",
-        poster: "http://image.yes24.com/momo/TopCate1291/MidCate003/129025467.jpg"
-      }
-    ]
   }
 
   componentDidMount() {
     setTimeout( () => {
       this.setState({
         movies: [
-          ...this.state.movies,
+          {
+            id: 1,
+            title: "Matrix",
+            poster: "http://ticketimage.interpark.com/Movie/still_image/V16/V1601447p_s01.gif"
+          },
+          {
+            id: 2,
+            title: "Full Metal Jacket",
+            poster: "https://images-na.ssl-images-amazon.com/images/I/41VXPrZfDXL.jpg"
+          },
+          {
+            id: 3,
+            title: "Old Boy",
+            poster: "https://images-na.ssl-images-amazon.com/images/I/51U%2BG%2BZgGgL.jpg"
+          },
           {
             id: 4,
             title: "Star Wars",
@@ -47,16 +38,21 @@ class App extends Component {
     }, 5000)
   }
 
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    })
+    return movies
+  }
+
   render() {
     return (
       <div className="App">
-        {this.state.greeting}
-        {this.state.movies.map((movie, index) => {
-          return <Movie title={movie.title} poster={movie.poster} key={index} />
-        })}
+        {this.state.movies ? this._renderMovies() : 'Loading'}
       </div>
     );
   }
+
 }
 
 export default App;
