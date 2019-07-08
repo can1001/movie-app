a37073888e1d79949c21399aa418068309fb6623
can1001 <hyejong.jeong@gmail.com>
2019-07-09 02:37:38 +0900

    Updates

---
+++ precache-manifest.fdc5acf95f29c20402686d7cf2026fd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25174a19d2ff1f9e97ee5e09ec564f72",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "50168924ae819f8111e0",
+    "url": "/movie-app/static/css/main.602e9502.chunk.css"
+  },
+  {
+    "revision": "e083292f352b8a48b197",
+    "url": "/movie-app/static/js/2.61ebbd48.chunk.js"
+  },
+  {
+    "revision": "50168924ae819f8111e0",
+    "url": "/movie-app/static/js/main.3551717e.chunk.js"
+  },
+  {
+    "revision": "3a5315027bacbafbe345",
+    "url": "/movie-app/static/js/runtime~main.7dd63718.js"
+  }
+]);
